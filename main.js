d739a02cd45440c29390cc525e996dcf46eda8c8
Seth Romanowski <33106212+sromanowski12@users.noreply.github.com>
2018-06-27 00:45:14 -0400

    added menu and html template

--- main.js
+++ main.js
@@ -1,50 +1,123 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const electron = require('electron');
+const {
+  app,
+  BrowserWindow,
+  Menu
+} = require('electron');
 
 // Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+// be closed automatically when the javascript object is garbage collected.
+let mainWindow;
 
-function createWindow () {
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  if (process.platform != 'darwin')
+    app.quit();
+});
+
+// This method will be called when Electron has done everything
+// initialization and ready for creating browser windows.
+app.on('ready', function () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  var application_menu = [{
+    label: 'menu1',
+    submenu: [{
+        label: 'Undo',
+        accelerator: 'CmdOrCtrl+Z',
+        role: 'undo'
+      },
+      {
+        label: 'Open',
+        accelerator: 'CmdOrCtrl+O',
+        click: () => {
+          electron.dialog.showOpenDialog({
+            properties: ['openFile', 'openDirectory', 'multiSelections']
+          });
+        }
+      },
+      {
+        label: 'submenu1',
+        submenu: [{
+            label: 'item1',
+            accelerator: 'CmdOrCtrl+A',
+            click: () => {
+              mainWindow.openDevTools();
+            }
+          },
+          {
+            label: 'item2',
+            accelerator: 'CmdOrCtrl+B',
+            click: () => {
+              mainWindow.closeDevTools();
+            }
+          }
+        ]
+      }
+    ]
+  }];
+  if (process.platform == 'darwin') {
+    const name = app.getName();
+    application_menu.unshift({
+      label: name,
+      submenu: [{
+          label: 'About ' + name,
+          role: 'about'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Services',
+          role: 'services',
+          submenu: []
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Hide ' + name,
+          accelerator: 'Command+H',
+          role: 'hide'
+        },
+        {
+          label: 'Hide Others',
+          accelerator: 'Command+Shift+H',
+          role: 'hideothers'
+        },
+        {
+          label: 'Show All',
+          role: 'unhide'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Quit',
+          accelerator: 'Command+Q',
+          click: () => {
+            app.quit();
+          }
+        },
+      ]
+    });
+  }
+
+  menu = Menu.buildFromTemplate(application_menu);
+  Menu.setApplicationMenu(menu);
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
-  })
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+    mainWindow = null;
+  });
+});
